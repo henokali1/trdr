c93e6d1d51e0eb80a045bb8e5fffd6f4edd6c390
henokali1 <henokali1@gmail.com>
2022-12-03 17:36:05 +0400

    50

--- neural_network_pine_to_py/pg.py
+++ neural_network_pine_to_py/pg.py
@@ -41,16 +41,32 @@ m_masell = get_masell(m_out111)
 
 # 5 EMAs
 len1 = 9
+len2 = 21
+len3 = 55
+len4 = 100
+len5 = 200
 m_src1 = close_price
+m_src2 = close_price
+m_src3 = close_price
+m_src4 = close_price
+m_src5 = close_price
 m_out1 = ta.ema(tv_df['close'], len1)
+m_out2 = ta.ema(tv_df['close'], len2)
+m_out3 = ta.ema(tv_df['close'], len3)
+m_out4 = ta.ema(tv_df['close'], len4)
+m_out5 = ta.ema(tv_df['close'], len5)
+
+# Supertrend
+
+
 
 exp_df = pd.DataFrame()
 exp_df['open'] = open_price
 exp_df['high'] = high_price
 exp_df['low'] = low_price
 exp_df['close'] = close_price
-exp_df['out1'] = list(tv_df['out1'])
-exp_df['m_out1'] = m_out1
+exp_df['out5'] = list(tv_df['out5'])
+exp_df['m_out5'] = m_out5
 
 exp_fn = 'exp_df.csv'
 exp_df.to_csv(exp_fn, index=False)
