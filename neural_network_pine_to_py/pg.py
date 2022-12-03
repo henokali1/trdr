b9ef7ed908c8110cb1435e9f40427339ef6d2f40
henokali1 <henokali1@gmail.com>
2022-12-03 17:16:37 +0400

    21

--- neural_network_pine_to_py/pg.py
+++ neural_network_pine_to_py/pg.py
@@ -4,6 +4,22 @@ import pandas as pd
 import pandas_ta as ta
 from pathlib import Path
 
+
+def get_mabuy(m_out111):
+    r=[0]
+    for idx in range(len(m_out111)):
+        if idx > 0:
+            r.append(int(m_out111[idx] > m_out111[idx-1]))
+    return r
+
+def get_masell(m_out111):
+    r=[0]
+    for idx in range(len(m_out111)):
+        if idx > 0:
+            r.append(int(m_out111[idx] < m_out111[idx-1]))
+    return r
+ 
+
 downloads_path = str(Path.home() / "Downloads")
 tv_exp_fn = f'{downloads_path}\\BINANCE_BTCUSDTPERP, 3.csv'
 
@@ -18,19 +34,23 @@ close_price = list(tv_df['close'])
 # Trend EMA
 tradetrendoption = False
 len111 = 200
-src111 = tv_df
+m_src111 = close_price
 m_out111 = ta.ema(tv_df['close'], len111)
-# ma111  = plot(out111, title="EMA 200", linewidth=2, color=color.blue, offset=0)
-# mabuy  = out111 > out111[1]
-# masell = out111 < out111[1]
+m_mabuy = get_mabuy(m_out111)
+m_masell = get_masell(m_out111)
+
+# 5 EMAs
+len1 = 9
+m_src1 = close_price
+m_out1 = ta.ema(tv_df['close'], len1)
 
 exp_df = pd.DataFrame()
 exp_df['open'] = open_price
 exp_df['high'] = high_price
 exp_df['low'] = low_price
 exp_df['close'] = close_price
-exp_df['out111'] = list(tv_df['out111'])
-exp_df['m_out111'] = m_out111
+exp_df['out1'] = list(tv_df['out1'])
+exp_df['m_out1'] = m_out1
 
 exp_fn = 'exp_df.csv'
 exp_df.to_csv(exp_fn, index=False)
