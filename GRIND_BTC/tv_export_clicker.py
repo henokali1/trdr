581bd10afd6c4f2b501ff9fab5b23d89313cfa5a
henokali1 <henokali1@gmail.com>
2023-07-19 10:01:49 +0400

    SAR done

--- GRIND_BTC/tv_export_clicker.py
+++ GRIND_BTC/tv_export_clicker.py
@@ -13,16 +13,6 @@ def print_mouse_coordinates():
         print(f"X: {x} Y: {y}")
         sleep(1)
 
-sleep(2)
-move_mouse_to_coordinates(1347, 125)
-pyautogui.click()
-sleep(1)
-move_mouse_to_coordinates(1446, 320)
-pyautogui.click()
-sleep(1)
-move_mouse_to_coordinates(941, 611)
-sleep(1)
-pyautogui.click()
 
 def on_key_press(event):
     if event.name == 'esc':
@@ -42,8 +32,17 @@ def on_key_press(event):
         move_mouse_to_coordinates(939, 587)
         sleep(0.5)
         pyautogui.click()
-        sleep(0.25)
+        sleep(1)
         keyboard.press_and_release('alt+tab')
+        sleep(0.1)
+        keyboard.press('ctrl')
+        keyboard.press('enter')
+        sleep(0.1)  # Optional delay between key presses (adjust as needed)
+        keyboard.release('enter')
+        keyboard.release('ctrl')
+        sleep(7)
+        move_mouse_to_coordinates(1023, 215)
+        pyautogui.click()
 
                 
 
