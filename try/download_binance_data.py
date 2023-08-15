99564789312182b1973481f674dc2359a7d8265f
henokali1 <henokali1@gmail.com>
2023-08-15 11:08:05 +0400

    download 1m data since 2019 11

--- try/download_binance_data.py
+++ try/download_binance_data.py
@@ -3,15 +3,10 @@ from binance.enums import *
 from time import time
 import pickle as pickle
 from datetime import datetime
-from os import listdir
-from os.path import isfile, join
-import unicorn_binance_websocket_api
-import threading
-import ast
 import pandas as pd
 
 def get_client():
-    fn = '/home/azureuser/key/binance-key.pickle'
+    fn = 'C:\\Users\\HK\\Documents\\Bkey\\binance-key.pickle'
     with open(fn, 'rb') as handle:
         k = pickle.load(handle)
     return Client(k['API_KEY'], k['API_SECRET'])
@@ -38,33 +33,6 @@ def get_unix_timestamp(date_string):
         print("Invalid date format. Please use the format 'dd/mm/yyyy hh:mm:ss'.")
         return None
 
-
-def heikin_ashi(df):
-    """
-    Converts the input DataFrame into Heikin Ashi candles and returns it as a new DataFrame.
-
-    Parameters:
-        df (pandas.DataFrame): Input DataFrame with columns ['timestamp', 'open', 'high', 'low', 'close', 'volume'].
-
-    Returns:
-        pandas.DataFrame: DataFrame with Heikin Ashi candles and columns ['timestamp', 'open', 'high', 'low', 'close', 'volume'].
-    """
-    ha_df = pd.DataFrame(index=df.index, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'real_open'])
-    tot = len(df)
-    for i in range(tot):
-        if i == 0:
-            ha_df.iloc[i] = df.iloc[i]
-        else:
-            ha_df.at[i, 'time'] = df.at[i, 'time']
-            ha_df.at[i, 'open'] = (ha_df.at[i - 1, 'open'] + ha_df.at[i - 1, 'close']) / 2
-            ha_df.at[i, 'close'] = (df.at[i, 'open'] + df.at[i, 'high'] + df.at[i, 'low'] + df.at[i, 'close']) / 4
-            ha_df.at[i, 'high'] = max(df.at[i, 'high'], ha_df.at[i, 'open'], ha_df.at[i, 'close'])
-            ha_df.at[i, 'low'] = min(df.at[i, 'low'], ha_df.at[i, 'open'], ha_df.at[i, 'close'])
-            ha_df.at[i, 'volume'] = df.at[i, 'volume']
-            ha_df.at[i, 'real_open'] = df.at[i, 'real_open']
-
-    return ha_df
-
 def get_historical_data(start_timestamp, end_timestamp): 
     data = []
     tot = (end_timestamp - start_timestamp)/(900*500)
@@ -73,7 +41,7 @@ def get_historical_data(start_timestamp, end_timestamp):
         next_ets = current_sts + 900*500 if (current_sts + 900*500) < end_timestamp else end_timestamp
         print(current_sts, next_ets, f'100% completed') if next_ets == end_timestamp else print(current_sts, next_ets, f'{round(cntr*100/tot, 1)}% completed')
         cntr += 1
-        klines = client.futures_historical_klines('BTCUSDT', '15m', current_sts*1000, next_ets*1000, limit=500)
+        klines = client.futures_historical_klines('BTCUSDT', '1m', current_sts*1000, next_ets*1000, limit=500)
         
         for kline in klines:
             timestamp = kline[0]/1000
@@ -83,19 +51,14 @@ def get_historical_data(start_timestamp, end_timestamp):
             close_price = float(kline[4])
             volume = float(kline[5])
 
-            data.append([timestamp, open_price, high_price, low_price, close_price, volume, open_price])
-    print('Converting to Heikin Ashi...')
+            data.append([timestamp, open_price, high_price, low_price, close_price, volume])
 
-    df = pd.DataFrame(data, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'real_open'])
-    # df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    # hdf = heikin_ashi(df)
-    hdf = df
-    # hdf.to_csv(f'{start_timestamp}--{end_timestamp}.csv', index=False)
-    hdf.to_csv(f'tst.csv', index=False)
+    df = pd.DataFrame(data, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+    df.to_csv(f'tst.csv', index=False)
     print('Data Exported')
 
 
-start_timestamp = get_unix_timestamp('01/10/2019 00:00:00')
+start_timestamp = get_unix_timestamp('1/11/2019 00:00:00')
 end_timestamp = int(time())
 
 
