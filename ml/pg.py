7a09b6bf901424d7e74c22e890c970a41af128f9
henokali1 <henokali1@gmail.com>
2023-10-01 18:23:39 +0400

    .

--- ml/pg.py
+++ ml/pg.py
@@ -9,34 +9,34 @@ from sklearn.impute import SimpleImputer
 
 
 exp = ClassificationExperiment()
-# data = pd.read_csv('/home/hk_sc/data/BTCUSDT-dataset.csv')
-# coins = list(data['coin'])
-# data['signal'].replace({'dont_trade': 0, 'long': 1, 'short': 2}, inplace=True)
+data = pd.read_csv('/home/hk_sc/data/ETHUSDT-dataset.csv')
+coin = list(data['coin'])[0]
+data['signal'].replace({'dont_trade': 0, 'long': 1, 'short': 2}, inplace=True)
 
-# data = data.select_dtypes(include=['number'])  # This keeps only numeric columns
+data = data.select_dtypes(include=['number'])  # This keeps only numeric columns
 
-# # Separate the features (X) and the target variable (y)
-# X = data.drop(columns=['signal'])
-# y = data['signal']
+# Separate the features (X) and the target variable (y)
+X = data.drop(columns=['signal'])
+y = data['signal']
 
-# # Handle missing values in X using SimpleImputer (replace NaN with the mean of each column)
-# imputer = SimpleImputer(strategy='mean')
-# X = imputer.fit_transform(X)
+# Handle missing values in X using SimpleImputer (replace NaN with the mean of each column)
+imputer = SimpleImputer(strategy='mean')
+X = imputer.fit_transform(X)
 
-# # Initialize the SMOTE object
-# smote = SMOTE(random_state=42)
+# Initialize the SMOTE object
+smote = SMOTE(random_state=42)
 
-# # Apply SMOTE to balance the dataset
-# X_resampled, y_resampled = smote.fit_resample(X, y)
+# Apply SMOTE to balance the dataset
+X_resampled, y_resampled = smote.fit_resample(X, y)
 
-# # Create a new balanced dataframe
-# data = pd.concat([pd.DataFrame(X_resampled, columns=data.columns[:-1]), pd.DataFrame({'signal': y_resampled})], axis=1)
+# Create a new balanced dataframe
+data = pd.concat([pd.DataFrame(X_resampled, columns=data.columns[:-1]), pd.DataFrame({'signal': y_resampled})], axis=1)
 # data.to_csv('/home/hk_sc/data/balanced_data.csv', index=False)
-data = pd.read_csv('/home/hk_sc/data/balanced_data.csv')
+# data = pd.read_csv('/home/hk_sc/data/balanced_data.csv')
 
 data.replace([np.inf, -np.inf], np.nan, inplace=True)
-data['coin'] = ['BTCUSDT']*318093
-print(len(list(data['signal'])))
+data['coin'] = [coin]*len(data['signal'])
+
 s = setup(data, target = 'signal', categorical_features=['coin'], session_id = 123, use_gpu=True)
 model = create_model(LGBMClassifier())
 validation_scores = pull()
