840b1a7f23450d89f55301e269fe9d5ccf52be83
henokali1 <henokali1@gmail.com>
2023-10-19 07:35:37 +0400

    .

--- ml/pg.py
+++ ml/pg.py
@@ -1,22 +1,22 @@
-import matplotlib.pylab as plt
-import os
-
-
-
-from tsfresh import extract_features, extract_relevant_features, select_features
-from tsfresh.utilities.dataframe_functions import impute
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-
 import pandas as pd
 import numpy as np
+import pandas_ta as ta
+from pycaret.classification import load_model, predict_model
 
-df = pd.read_csv(os.path.abspath('../data/df.csv'))
-y = pd.read_csv(os.path.abspath('../data/y.csv'))
 
-df["id"] = df.index
-df = df.melt(id_vars="id", var_name="time").sort_values(["id", "time"]).reset_index(drop=True)
-X = extract_features(df[df["id"] < len(df)], column_id="id", column_sort="time", impute_function=impute)
-X.to_csv(os.path.abspath('../data/X.csv'), index=False)
+
+model_name = f'model_ta_feats'
+model = load_model(f'/home/hk_sc/models/{model_name}')
+plot_model(model, plot = 'confusion_matrix', plot_kwargs = {'percent': True}, save=True)
+
+data = pd.read_csv('/home/hk_sc/data/ta_feats.csv')
+
+correct_signals = list(data['signal'])
+predictions = predict_model(model, data)
+
+exp_df = pd.DataFrame()
+exp_df['prediction_score'] = predictions['prediction_score']
+exp_df['correct_signals'] = correct_signals
+exp_df['signal'] = predictions['signal']
+exp_df['prediction_label'] = predictions['prediction_label']
+exp_df.to_csv('/home/hk_sc/data/predictions.csv')
